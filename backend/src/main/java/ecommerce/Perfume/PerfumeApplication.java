d891eadc5c1c6e22219a6a0aa4eca20a30d4f67f
kidneyflowerSE <ilovehtmlandcss@gmail.com>
2024-12-06 10:33:24 +0700

    first commit

---
+++ backend/src/main/java/ecommerce/Perfume/PerfumeApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.Perfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeApplication.class, args);
+	}
+
+}
